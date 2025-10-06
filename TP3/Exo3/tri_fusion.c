3649c264fc58b2ceea1d2e8e8dbfd681a43b4c42
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-06 17:30:56 +0100

    tp3, ex3, tri fusion: implemeted

--- TP3/Exo3/tri_fusion.c
+++ TP3/Exo3/tri_fusion.c
@@ -5,20 +5,121 @@
 /**
  * Recherche récursive d'une valeur encadrée dans un tableau trié
  * @param tab tableau d'entiers
- * @param res tableau d'entiers vide
+ * @param debut indice de debut
+ * @param fin indice de fin
  */
-int[] copier_tableau(int[] tab, int[] res);
+int *copier_tableau(int[] tab, int debut, int fin) {
+	int size = fin - debut;
+	int *res = malloc(size * sizeof(int));
+	
+	if (res == NULL) {
+		perror("Erreur d'allocation mémoire");
+		exit(EXIT_FAILURE);
+	}
+	
+	for(int i = 0 ; i < size ; i++) {
+		res[i] = tab[debut + i];
+	}
+	
+	return res;
+}
 
 /**
  * Recherche récursive d'une valeur encadrée dans un tableau trié
  * @param gauche tableau d'entiers trié par ordre croissant
  * @param droite tableau d'entiers trié par ordre croissant
  */
-int fusionner(int[] gauche, int[] droite);
+int *fusionner(int[] gauche, int size_gauche, int[] droite, int size_droite) {
+	int size = size_droite + size_gauche;
+	
+	int *res = malloc(size * sizeof(int));
+	if (res == NULL) {
+		perror("Erreur d'allocation mémoire");
+		exit(EXIT_FAILURE);
+	}
+	
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	
+	while ((i < size_gauche) && (j < size_droite)) {
+		if (gauche[i] < droite[j]) {
+			res[k] = gauche[i];
+			i++;
+		} else {
+			res[k] = droite[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while (i < size_gauche) {
+		res[k] = gauche[i];
+		i++;
+		k++;
+	}
+
+	while (j < size_droite) {
+		res[k] = droite[j];
+		j++;
+		k++;
+	}
+	
+	return res;
+}
 
 /**
  * Recherche linéaire naïve d'une valeur encadrée (pour comparaison)
  * @param tab tableau d'entiers
+ * @param size taille du tableau tab
  */
-int[] tri_fusion(int[] tab);
+int *tri_fusion(int[] tab, int size) {
+
+	if (size <= 1) {
+		return tab;
+	}
+	
+	int milieu = size / 2;
+	
+	int *gauche = copier_tableau(tab, 0, milieu);
+	int *droite = copier_tableau(tab, milieu, size);
+	
+	int size_gauche = sizeof(gauche) / sizeof(int);
+	int size_droite = sizeof(droite) / sizeof(int);
+	
+	gauche = tri_fusion(gauche, size_gauche);
+	droite = tri_fusion(droite, size_droite);
+	
+	int *res = fusionner(gauche, droite);
+	
+	free(gauche);
+	free(droite);
+	
+	return res;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- TP3/Exo3/tri_fusion.h
+++ TP3/Exo3/tri_fusion.h
@@ -1,19 +1,22 @@
-#ifndef TRI_FUSION.H
-#define TRI_FUSION.H
+#ifndef TRI_FUSION_H
+#define TRI_FUSION_H
 
 /**
  * Recherche récursive d'une valeur encadrée dans un tableau trié
  * @param tab tableau d'entiers
- * @param res tableau d'entiers vide
+ * @param debut indice de debut
+ * @param fin indice de fin
  */
-int[] copier_tableau(int[] tab, int[] res);
+int *copier_tableau(int[] tab, int debut, int fin)
 
 /**
  * Recherche récursive d'une valeur encadrée dans un tableau trié
  * @param gauche tableau d'entiers trié par ordre croissant
+ * @param size_gauche taille du tableau gauche
  * @param droite tableau d'entiers trié par ordre croissant
+ * @param size_droite taille du tableau droite
  */
-int fusionner(int[] gauche, int[] droite);
+int fusionner(int[] gauche, int size_gauche, int[] droite, int size_droite);
 
 /**
  * Recherche linéaire naïve d'une valeur encadrée (pour comparaison)
