ad568d19891677e50a3c185d0f92727b6175456f
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-06 17:38:25 +0100

    tp3, ex3, tri insertion: implemented

--- TP3/Exo3/tri_insertion.c
+++ TP3/Exo3/tri_insertion.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include "tri_insertion.h"
 
-void tri_insertion(int[] tab, int size) {
+void tri_insertion(int *tab, int size) {
 	int i,j;
 	int Clef;
 
-	for(i = 1 ; i < n ; i++) {
+	for(i = 1 ; i < size ; i++) {
 
-		Clef = t[i];
+		Clef = tab[i];
 		j = i - 1;
 
     		/* Decalage des valeurs du tableau */
-		while((j >= 0) && comp(t[j], Clef)) {
+		while((j >= 0) && (tab[j] > clef)) {
 			// cmp++ ;
-			t[j+1] = t[j];
+			tab[j+1] = tab[j];
 			j = j - 1;
 		}
 		// cmp += 2 ;
 
 		/* insertion de la clef */
-		t[j+1] = Clef;
+		tab[j+1] = Clef;
 	}
 	// cmp++ ;
 //	printf("%d comparaisons ont été faites\n", cmp) ;
