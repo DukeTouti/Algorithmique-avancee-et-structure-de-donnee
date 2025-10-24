e7e0406c64aff9714dd2d3175c09531baa244d92
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-24 22:17:38 +0100

    tp4, arbres binaires de recherches, abr_affichage.h: implemented

---
+++ TP4/abr_affichage.h
@@ -0,0 +1,12 @@
+#ifndef ABR_AFFICHAGE_H
+#define ABR_AFFICHAGE_H
+
+#include "types_abr.h"
+
+/* Affichage d'un element */
+void affiche_element(element_t e);
+
+/* Affichage de l'arbre en mode texte */
+void afficher_arbre(ab_t a);
+
+#endif
