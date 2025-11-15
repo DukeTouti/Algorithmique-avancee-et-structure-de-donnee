7179105383f4f37c87789481caf565e0f833f23a
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-11-15 20:45:37 +0100

    tp7, import des fichiers du tp6 à reutiliser

---
+++ TP7/tas_operations.h
@@ -0,0 +1,32 @@
+#ifndef TAS_OPERATIONS_H
+#define TAS_OPERATIONS_H
+
+#include "types_tas.h"
+#include "tas_utils.h"
+
+/* Fonction qui restaure la propriété de tas max à partir du noeud d'indice i */
+void entasserMax(noeud x, int i);
+
+/* Fonction qui restaure la propriété de tas min à partir du noeud d'indice i */
+void entasserMin(noeud x, int i);
+
+/* Fonction qui construit un tas max à partir du tas x */
+void construireTasMax(noeud x);
+
+/* Fonction qui construit un tas min à partir du tas x */
+void construireTasMin(noeud x);
+
+/* Fonction qui construit un tas max par insertion (élément par élément) */
+void construireTasMaxInsertion(noeud x);
+
+/* Fonction qui construit un tas min par insertion (élément par élément) */
+void construireTasMinInsertion(noeud x);
+
+/* Fonction qui tri récursivement un tas max (élément par élément) */
+void triTas(int *tab,int taille);
+
+void augmenterArbre(noeud x);
+
+void diminuerArbre(noeud x);
+
+#endif
