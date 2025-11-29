cdaa65083a88abbb0d0c12f340d01dfd8b4cb64f
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-11-29 19:29:54 +0100

    tp9, graphe.h: updated pour Prim et kruskal

--- TP9/graphe.h
+++ TP9/graphe.h
@@ -1,27 +1,37 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-/* Structure d'un noeud dans la liste d'adjacence */
+/* Structure pour représenter une arête pondérée (pour Kruskal) */
+typedef struct Arete {
+	int src ;
+	int dest ;
+	int poids ;
+} Arete;
+
+
+/* Structure d'un noeud dans la liste d'adjacence (pour Prim) */
 typedef struct Noeud {
-	int sommet ;
-	struct Noeud* suivant ;
+	int sommet ;              /* Sommet adjacent */
+	int poids ;               /* Poids de l'arête */
+	struct Noeud* suivant ;   /* Noeud suivant */
 } Noeud;
 
 /* Structure du graphe */
 typedef struct Graphe {
 	int nombreSommets ;       /* Nombre de sommets */
-	Noeud** listesAdjacence ; /* Tableau de listes d'adjacence */
-	int* visite ;             /* Tableau pour marquer les sommets visités (utilisé par BFS/DFS) */
+	int nombreAretes ;        /* Nombre d'arêtes */
+	Noeud** listesAdjacence ; /* Tableau de listes d'adjacence (pour Prim) */
+	Arete* aretes ;           /* Tableau de toutes les arêtes (pour Kruskal) */
+	int capaciteAretes ;      /* Capacité du tableau aretes */
 } Graphe;
 
 /* Fonctions de base pour le graphe */
-Noeud* creerNoeud(int sommet) ;
 Graphe* creerGraphe(int nombreSommets) ;
-void ajouterArrete(Graphe* graphe, int src, int dest) ;
+void ajouterArete(Graphe* graphe, int src, int dest, int poids) ;
 void afficherGraphe(Graphe* graphe) ;
 void libererGraphe(Graphe* graphe) ;
 
 /* Fonction pour réinitialiser le tableau visite */
-void reinitialiserVisite(Graphe* graphe) ;
+Graphe* genererGrapheAleatoire(int nombreSommets, double densite, int poidsMin, int poidsMax);
 
 #endif
